c2307a5fb61094340d3770909b628d81c2af814d
Emre KESIM <91027053+emrekesimemre@users.noreply.github.com>
2024-12-27 18:34:48 +0300

    feat: first comment

---
+++ app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -0,0 +1,24 @@
+import { notFound } from 'next/navigation';
+
+import { getNewsItem } from '@/lib/news';
+import BackDrop from '@/components/modal-backdrop';
+
+export default async function InterceptedImagePage({ params }) {
+  const newsItemSlug = params.slug;
+  const newsItem = await getNewsItem(newsItemSlug);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <>
+      <BackDrop />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
+    </>
+  );
+}
